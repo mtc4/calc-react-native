149849fa29b2fa82037ad6baff29f4840a94f679
mateusz <5410892+mtc4@users.noreply.github.com>
2018-11-08 10:55:14 +0100

    Style.js for Stylesheet

---
+++ Style.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+var Style = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
+	display: {
+		flex: 2, 
+		backgroundColor: '#1C1C1C'
+	}
+	buttons: {
+		flex: 8, 
+		backgroundColor: '#505050'
+	}
+});
+export default Style;
