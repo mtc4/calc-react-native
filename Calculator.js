79c0c02938b70a5d183ca24828db6df2200b6f56
mateusz <5410892+mtc4@users.noreply.github.com>
2018-11-08 10:52:52 +0100

    StyleSheet + Views

--- Calculator.js
+++ Calculator.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
 import {
     Text,
+    View,
     AppRegistry
 } from 'react-native';
 
 class Calculator extends Component {
 
-    render() {
-        return (
-            <Text>PROJECT LAB</Text>
-        )
-    }
+var Style = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
+	display: {
+		flex: 2, 
+		backgroundColor: '#1C1C1C'
+	}
+	buttons: {
+		flex: 8, 
+		backgroundColor: '#505050'
+	}
+});
+
+render() {
+    return (
+        <View style={Style.container}>
+            <View style={Style.display}></View>
+            <View style={Style.buttons}></View>
+        </View>
+    )
+}
 
 }
 
