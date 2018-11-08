ad492f804896fb19d219f356248180606ad92e8a
mateusz <5410892+mtc4@users.noreply.github.com>
2018-11-08 10:45:42 +0100

    Calculator component
    Calculator.js component initialization

---
+++ Calculator.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {
+    Text,
+    AppRegistry
+} from 'react-native';
+
+class Calculator extends Component {
+
+    render() {
+        return (
+            <Text>PROJECT LAB</Text>
+        )
+    }
+
+}
+
+AppRegistry.registerComponent('Calculator', () => Calculator);
