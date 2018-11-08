bca733108f146cee84099dab5bf72c4d43fd787f
mateusz <5410892+mtc4@users.noreply.github.com>
2018-11-08 11:04:59 +0100

    Rendering buttons
    Added buttons list

--- Calculator.js
+++ Calculator.js
@@ -4,7 +4,13 @@ import {
     View,
     AppRegistry
 } from 'react-native';
-
+import Button from './Buttons';
+const buttonsList = [
+    [1, 2, 3, '/'],
+    [4, 5, 6, '*'],
+    [7, 8, 9, '-'],
+    [0, '.', '=', '+']
+];
 class Calculator extends Component {
 
 import Style from './Style';
@@ -13,9 +19,34 @@ render() {
     return (
         <View style={Style.container}>
             <View style={Style.display}></View>
-            <View style={Style.buttons}></View>
+            <View style={Style.buttons}>
+		{this.renderButtons()}
+	    </View>
         </View>
     )
+}
+
+renderButtons() {
+
+        let views = [];
+
+        for (var r = 0; r < buttonsList.length; r ++) {
+            let row = buttonsList[r];
+
+            let row = [];
+            for (var i = 0; i < row.length; i ++) {
+                let input = row[i];
+
+                row.push(
+                    <Button value={input} key={r + "-" + i} />
+                );
+            }
+
+            views.push(<View style={{flex: 1, flexDirection: 'row'}} key={"row-" + r}>{row}</View>)
+        }
+
+        return views;
+
 }
 
 }
